41b0a90a4eb72ada1f6d4dce44187823367e59b5
F0rkster <matthias-gabel@hotmail.de>
2021-01-08 18:11:37 +0100

    wtf

---
+++ src/config/init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Matthias Gabel<matthias.gabel@fh-erfurt.de>
+ * @copyright Since 2021 by Matthias Gabel
+ * @version 1.0.0
+ */
+
+define('CONTROLLERSPATH',   'controllers' . DIRECTORY_SEPARATOR);
+define('COREPATH',          'core' . DIRECTORY_SEPARATOR);
+define('ASSETSPATH',        'assets' . DIRECTORY_SEPARATOR);
+define('MODELSPATH',        'models' . DIRECTORY_SEPARATOR);
+define('VIEWSPATH',         'views' . DIRECTORY_SEPARATOR);
