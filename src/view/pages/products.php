ff33a689743777dfbd930d019784348dfc924b90
MKnop2 <kickmepony@gmail.com>
2021-01-08 21:12:15 +0100

    shop / produkte verbesserungen

--- src/view/pages/products.php
+++ src/view/pages/products.php
@@ -11,49 +11,112 @@
 <!--    </nav>-->
 <div id="itemDisplay">
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item1.jpg' ?>" alt="item1">
+            <h2>Item 1</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item2.jpg' ?>" alt="item2">
+            <h2>Item 2</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+            </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item3.jpg' ?>" alt="item3">
+            <h2>Item 3</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item4.jpg' ?>" alt="item4">
+            <h2>Item 4</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item5.jpg' ?>" alt="item5">
+            <h2>Item 5</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item6.jpg' ?>" alt="item6">
+            <h2>Item 6</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item7.jpg' ?>" alt="item7">
+            <h2>Item 7</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item8.jpg' ?>" alt="item8">
+            <h2>Item 8</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
     <div class="item">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-        et ea rebum. Stet clita kasd gubergren,
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item9.jpg' ?>" alt="item9">
+            <h2>Item 9</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
+    </div>
+    <div class="item">
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item10.jpg' ?>" alt="item10">
+            <h2>Item 10</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
+    </div>
+    <div class="item">
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item11.jpg' ?>" alt="item11">
+            <h2>Item 11</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
+    </div>
+    <div class="item">
+        <article>
+            <img src="<?= ROOTPATH . 'assets/images/products/item12.jpg' ?>" alt="item12">
+            <h2>Item 12</h2>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren,
+        </article>
     </div>
 </div>
 
