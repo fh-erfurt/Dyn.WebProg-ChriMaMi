fcb1209c1c772775b8cb7d538133f1027190a6d1
MKnop2 <kickmepony@gmail.com>
2021-01-08 18:20:56 +0100

    warenkorb verschoben/ und google maps hinzugefügt

--- src/view/site.php
+++ src/view/site.php
@@ -98,7 +98,7 @@
         </div>
         <div class="right">
             <ul>
-                <li><img src="<?= ROOTPATH . '/assets/images/googleMaps.png' ?>"/></li>
+                <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2629.868419496588!2d11.523245315834837!3d48.76530901520996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479e5517a522c065%3A0x2ad0f38dd96ff57e!2sIngolst%C3%A4dter%20Str.%2039%2C%2085098%20Gro%C3%9Fmehring!5e0!3m2!1sde!2sde!4v1610125519667!5m2!1sde!2sde" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></li>
             </ul>
         </div>
     </section>
