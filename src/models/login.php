6b78322a6144f95ffd75a9d556e76df63796cbc1
F0rkster <matthias-gabel@hotmail.de>
2021-01-09 14:54:11 +0100

    add login.php

---
+++ src/models/login.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author Matthias Gabel<matthias.gabel@fh-erfurt.de>
+ * @copyright Since 2021 by Matthias Gabel
+ * @version 1.0.0
+ */
+
+
+namespace dwp\model;
+
+class Login extends \dwp\core\Model
+{
+    const TABLENAME = 'login';
+
+    protected $schema = [
+        'id' => [
+            'type' => parent::TYPE_UINTEGER,
+            'null' => false
+            ],
+        'passwordHash' => [
+            'type' => parent::TYPE_STRING,
+            'null' => false
+            ],
+        'email' => [
+            'type' => parent::TYPE_STRING,
+            'validate' => [
+                'min' => 2,
+                'max' => 25,
+                'null' => false,
+            ],
+        ],
+    ];
+}
