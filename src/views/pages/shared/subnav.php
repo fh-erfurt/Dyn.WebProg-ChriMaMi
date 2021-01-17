874fd9ae21cb9dd14ee98a35009dd76f175f3cd5
MKnop2 <kickmepony@gmail.com>
2021-01-17 18:21:14 +0100

    css: px -> em done

--- src/views/pages/shared/subnav.php
+++ src/views/pages/shared/subnav.php
@@ -1,7 +1,7 @@
 <div class="secondNavigation">
     <!--    <nav class="secondNavigation">>-->
     <ul class="secondNavigation">
-        <li><div class="text"><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=news">Aktuelle Projekte</a></div></li>
+        <li><div class="text"><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=news">Kontaktaufnahme</a></div></li>
         <li><div class="text"><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=categories">Shop</a></div></li>
         <li><label for="suche"></label><input type="search" id="suche" placeholder="  Suche"></li>
         <li><div class="icon"><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=cart"><img src="<?= ASSETSPATH . 'icons' . DIRECTORY_SEPARATOR . 'checkoutCart.png' ?>"/></a></div></li>
