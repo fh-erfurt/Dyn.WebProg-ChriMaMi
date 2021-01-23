046582b987fa6ad7445fe384d8b3309724c40db2
Chris <christoph.frischmuth@fh-erfurt.de>
2021-01-23 21:11:01 +0100

    - mobile navbar finalized - browser navbar already crashes

--- src/views/pages/shared/header.php
+++ src/views/pages/shared/header.php
@@ -7,9 +7,21 @@
             <h1>Ingenieurbüro</h1>
             <h2>Patrick Horsch</h2>
             <nav>
+               <!--Only for Mobile Version-->
                 <input class="menuToggle" type="checkbox" id="toggle">
                 <ul class="navigation">
-                    <li><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=main">Home</a></li>
+                    <div id="respSubNavHome" for="homeSubToggle">
+                        <li><label for="homeSubToggle" class="navigation"><!--<span>-->Unternehmen<!--</span>--></label>
+                            <input class="subMenuToggle" type="checkbox" id="homeSubToggle">
+                            <ul class="subNavigation" >
+                                <li><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=pages&a=main">Startseite</a></li>
+                                <li><a href="#">Über uns</a></li>
+                                <li><a href="#">Aktuelle Projekte</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                    <!--Only for Mobile Version-->
+
                     <li><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=shop&a=categories">Produkte</a></li>
                     <li><a href="<?= $_SERVER['SCRIPT_NAME'] ?>?c=shop&a=cart">Warenkorb</a></li>
                     <?php
@@ -19,18 +31,18 @@
                         if (isset($_SESSION['email']) && $_SESSION['isAdmin'] == 1) {
 /*                            echo '<li><a href="' . $_SERVER['SCRIPT_NAME'] . '?c=accounts&a=administration">Administration</a></li>';*/
                             echo'<div id="respSubNav" for="subToggle">';
-                            echo '<li><label for="subToggle" class="navigation">Administration</label></li>';
+                            echo '<li><label for="subToggle" class="navigation"><span>Administration</span></label>';
                             echo '<input class="subMenuToggle" type="checkbox" id="subToggle">';
-                            echo '</div>';
                             echo '<ul class="subNavigation">';
                                 echo '<li><a href="#">Benutzerverwaltung</a></li>';
-                            echo '</ul>';
+                            echo '</ul></li>';
+                            echo '</div>';
 
                         } else if (isset($_SESSION['email']) && $_SESSION['isAdmin'] == 0) {
                             echo '<li><a href="' . $_SERVER['SCRIPT_NAME'] . '?c=accounts&a=userInterface">Account</a>';
                             echo '<ul>';
                             echo '<div class="submenu">';
-                            echo '<div>';
+                         /*   echo '<div>';*/
                             echo '<li><a href="' . $_SERVER['SCRIPT_NAME'] . '?c=accounts&a=logout">Logout</a></li>';
                             echo '</div>';
                             echo '<div class="bigger">';
