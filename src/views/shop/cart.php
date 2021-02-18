a3ce8e3c95c27ef583fc21671bc2278b9b1ba255
F0rkster <matthias-gabel@hotmail.de>
2021-02-18 19:45:44 +0100

    nicht schön aber selten

--- src/views/shop/cart.php
+++ src/views/shop/cart.php
@@ -16,24 +16,28 @@
                 aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
                 kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>-->
-            <section class="cartItem">
-                <img src="<?= ASSETSPATH . 'images'. DIRECTORY_SEPARATOR. 'products'. DIRECTORY_SEPARATOR.'item1.jpg'?>" alt="item1">
-                <div class="descriptionAll">
-                    <p>Item_01</p>
-                        <ul>
-                            <li>Stk.-Preis: src="<?= ASSETSPATH . 'icons' . DIRECTORY_SEPARATOR . 'trash.png' ?></li>
-                            <li>Gesamtpreis: 2000€</li>
-                        </ul>
-                </div>
-                <div class="counter">
-                    <div class="box" data-type="decrease" id="leftBox"><a>-</a></div>
-                    <div class="box" data-type="amount"   id="centerBox"><a>0</a></div>
-                    <div class="box" data-type="increase" id="rightBox"><a>+</a></div>
-                </div>
-                <div class="iconImg">
-                    <img src="<?= ASSETSPATH . 'icons' . DIRECTORY_SEPARATOR . 'trash.png' ?> " alt="Entfernen"/>
-                </div>
-            </section>
+            <? foreach ($cart_view as $ware) :?>
+                <section class="cartItem">
+                    <img src="<?= ASSETSPATH . 'images'. DIRECTORY_SEPARATOR. 'products'. DIRECTORY_SEPARATOR.$ware->filename?>" alt="<?=$ware->alt_text?>">
+                    <div class="descriptionAll">
+                        <p>Item_01</p>
+                            <ul>
+<!--                                <li>Stk.-Preis: src="<?/*= ASSETSPATH . 'icons' . DIRECTORY_SEPARATOR . 'trash.png' */?></li>
+-->
+                                <li>Stk.-Preis: "<?=$ware->std_price?></li>
+                                <li>Gesamtpreis: <?=$ware->total_price?></li>
+                            </ul>
+                    </div>
+                    <div class="counter">
+                        <div class="box" data-type="decrease" id="leftBox"><a>-</a></div>
+                        <div class="box" data-type="amount"   id="centerBox"><a>0</a></div>
+                        <div class="box" data-type="increase" id="rightBox"><a>+</a></div>
+                    </div>
+                    <div class="iconImg">
+                        <img src="<?= ASSETSPATH . 'icons' . DIRECTORY_SEPARATOR . 'trash.png' ?> " alt="Entfernen"/>
+                    </div>
+                </section>
+            <? endforeach;?>
             <hr/>
         </section>
         <aside>
