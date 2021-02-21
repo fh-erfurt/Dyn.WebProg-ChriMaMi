6399a296711b9314030e2708ab98f5ce9f85873f
MKnop2 <kickmepony@gmail.com>
2021-02-21 20:07:47 +0100

    usermanagement 1.0

--- src/views/administration/usermanagement.php
+++ src/views/administration/usermanagement.php
@@ -2,8 +2,46 @@
     <?require_once SHAREDPATH . 'adminnav.php'; ?>
 
     <main>
-        <section>
-            <p> Here you can Manage the Users </p>
-        </section>
+        <form>
+            <h2> Finde und Verwalte Nutzer </h2>
+            <section id="managementTable">
+                <div class="tableHeader">
+                    <div class="userID"><ul>UserID</ul></div>
+                    <div class="email"><ul>Email</ul></div>
+                    <div class="firstname"><ul>Vorname</ul></div>
+                    <div class="lastname"><ul>Nachname</ul></div>
+                    <div class="gender"><ul>(m/w/d)</ul></div>
+                    <div class="role"><ul>Rolle</ul></div>
+
+                </div>
+                <div class="gray">
+                    <div class="userID"><ul>8</ul></div>
+                    <div class="email"><ul>keck@gmail.de</ul></div>
+                    <div class="firstname"><ul>Karl-Erik</ul></div>
+                    <div class="lastname"><ul>Cley</ul></div>
+                    <div class="gender"><ul>m</ul></div>
+                    <div class="role"><ul>user</ul></div>
+
+                </div>
+                <div class="white">
+                    <div class="userID"><ul>1</ul></div>
+                    <div class="email"><ul>matthias.gabel@fh-erfurt.de</ul></div>
+                    <div class="firstname"><ul>Matthias</ul></div>
+                    <div class="lastname"><ul>Gabel</ul></div>
+                    <div class="gender"><ul>m</ul></div>
+                    <div class="role"><ul>admin</ul></div>
+
+                </div>
+                <div class="gray">
+                    <div class="userID"><ul>4</ul></div>
+                    <div class="email"><ul>kristof.friess@fh-erfurt.de</ul></div>
+                    <div class="firstname"><ul>Kristof</ul></div>
+                    <div class="lastname"><ul>Friess</ul></div>
+                    <div class="gender"><ul>m</ul></div>
+                    <div class="role"><ul>user</ul></div>
+
+                </div>
+            </section>
+        </form>
     </main>
 </div>
