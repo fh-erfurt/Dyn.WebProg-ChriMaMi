311e763d6e019afad8cb9648e4af616e15e42ffc
F0rkster <matthias-gabel@hotmail.de>
2021-02-24 19:11:05 +0100

    fix

--- src/controllers/shopController.php
+++ src/controllers/shopController.php
@@ -97,7 +97,7 @@ class ShopController extends \dwp\core\Controller
         $this->setParam('result', $result);
 
 
-        $member = getCustomer($_SESSION['id']);
+        $member = getMember($_SESSION['id']);
         $address = getAddress($member->addresses_id);
 
         $this->setParam('member', $member);
@@ -109,7 +109,7 @@ class ShopController extends \dwp\core\Controller
 
             $ordersData = array(
                 'status'       => 'ordered',
-                'order_date'   => date(DATE_RFC822),
+                'order_date'   => date("Y-m-d H:i:s"),
                 'addresses_id' => $member->addresses_id,
                 'members_id'   => $member->id);
             $orders = new Orders($ordersData);
