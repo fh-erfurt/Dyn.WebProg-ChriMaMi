104f392ec03fdffd883aa6fbd89b299cd8ad6a54
Chris <christoph.frischmuth@fh-erfurt.de>
2021-02-24 15:06:45 +0100

    ++ shop änderungen

--- src/views/pages/shared/subnav.php
+++ src/views/pages/shared/subnav.php
@@ -8,11 +8,32 @@ if (!file_exists(ASSETSPATH . 'JSON/products.json')) {
         $jsonArray[$key] = array(
             'id' => $allProducts[$key]->id,
             'name' => $allProducts[$key]->name,
-            'category' => $allProducts[$key]->category,
             'description' => $allProducts[$key]->description,
-            'std_price' => $allProducts[$key]->std_price
-
+            'std_price' => $allProducts[$key]->std_price,
+            'picture' => $allProducts[$key]->filename,
+            'alt_text' => $allProducts[$key]->alt_text,
+            'category' => $allProducts[$key]->category
         );
+        echo var_dump([$key]['category']);
+        exit();
+
+        switch ($jsonArray[$key]['category']) {
+            case "fire_protection":
+                $jsonArray[$key]['category'] = 'Brandschutz';
+                break;
+            case "heat_protection":
+                $jsonArray[$key]['category'] = 'Wärmeschutz';
+                break;
+            case "structural_planning":
+                $jsonArray[$key]['category'] = 'Tragwerkplanung';
+                break;
+            case "input_planning":
+                $jsonArray[$key]['category'] = 'Eingabeplaung';
+                break;
+            default:
+                $jsonArray[$key]['category'] = 'Unbekannte Kategorie';
+                break;
+        }
     }
     $json = json_encode($jsonArray);
     $file = file_put_contents(ASSETSPATH . 'json/products.json', $json);
